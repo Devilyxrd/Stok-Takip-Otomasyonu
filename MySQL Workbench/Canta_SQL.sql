0af135ea2a1caaa4836b902eacddea8ecbc9ca74
Devil <82479805+Devilyxrd@users.noreply.github.com>
2022-05-23 21:43:04 +0300

    Add files via upload

---
+++ MySQL Workbench/Canta_SQL.sql
@@ -0,0 +1,17 @@
+use canta;
+
+select kategori , cinsiyet , marka , beden , renk , fiyat , materyal from urun;
+
+insert into urun(kategori, cinsiyet, marka, beden, renk, fiyat, materyal) values("Sırt Cantasi" , "Erkek" , "Gucci" , "M" , "Siyah" , "140Tl" , "Deri");
+
+
+
+
+
+
+
+/*select * from musteri;
+
+delete from musteri where idmusteri = 3;
+
+insert into musteri(ad, soyad, tc, pass) values(1 , "Selman" , "Dalıcı" , '11111111111' , "123");*/
